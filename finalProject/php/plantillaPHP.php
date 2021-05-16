d25d100d119f74e80a8a50fd30b9316b93bdef7b
Noemí Ortega <noemiortegacordoba@gmail.com>
2021-05-16 20:42:22 +0200

    r
    r

--- finalProject/php/plantillaPHP.php
+++
@@ -1,3 +0,0 @@
-<?php
- // Code PHP here
-?>
--- finalProject/scripts/loader.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function() { 
-	$('.preloader').fadeOut(); 
-	$('#preloader').delay(550).fadeOut('slow'); 
-	$('body').delay(450).css({'overflow':'visible'});
-});
