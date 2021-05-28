36f97ce666bd5cb119232a5610e8fee3912dd01a
Noemí Ortega <noemiortegacordoba@gmail.com>
2021-05-28 17:54:55 +0200

    z
    z

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,122 +1,34 @@
-(function($) {
-    "use strict";
+
+$(window).one('load', function() { 
 	
-	/* ..............................................
-	Loader 
-    ................................................. */
-	
-	$(window).on('load', function() { 
+    if (!readCookie("adSeen")) {
 		$('.preloader').fadeOut(); 
 		$('#preloader').delay(550).fadeOut('slow'); 
 		$('body').delay(450).css({'overflow':'visible'});
-	});
-    	
-	/* ..............................................
-    Navbar Bar
-    ................................................. */
 	
-	$('.navbar-nav .nav-link').on('click', function() {
-		var toggle = $('.navbar-toggler').is(':visible');
-		if (toggle) {
-			$('.navbar-collapse').collapse('hide');
-		}
-	});
+		createCookie("adSeen", "1", 1000);
+	}	
 	
-	/* ..............................................
-    Fixed Menu
-    ................................................. */
-    
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 50) {
-			$('.top-header').addClass('fixed-menu');
-		} else {
-			$('.top-header').removeClass('fixed-menu');
-		}
-	});
+});
+	
+	
+	
+function createCookie(name, value, days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+	} else var expires = "";
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-	/* ..............................................
-    Properties Filter
-    ................................................. */
-	var Container = $('.container');
-	Container.imagesLoaded(function () {
-		var portfolio = $('.properties-menu');
-		portfolio.on('click', 'button', function () {
-			$(this).addClass('active').siblings().removeClass('active');
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({
-				filter: filterValue
-			});
-		});
-		var $grid = $('.properties-list').isotope({
-			itemSelector: '.properties-grid'
-		});
-
-	});
-
-	/* ..............................................
-    Gallery
-    ................................................. */
-	
-	$(document).ready(function() {
-		$('.popup-gallery').magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			tLoading: 'Loading image #%curr%...',
-			mainClass: 'mfp-img-mobile',
-			gallery: {
-				enabled: true,
-				navigateByImgClick: true,
-				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-			},
-			image: {
-				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
-					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-				}
-			}
-		});
-	});
-	
-	/* ..............................................
-    Scroll To Top
-    ................................................. */
-	
-	$(document).ready(function () {
-
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 100) {
-				$('#scroll-to-top').fadeIn();
-			} else {
-				$('#scroll-to-top').fadeOut();
-			}
-		});
-
-		$('#scroll-to-top').click(function () {
-			$("html, body").animate({
-				scrollTop: 0
-			}, 600);
-			return false;
-		});
-
-	});
-	
-	
-	/* ..............................................
-    Smooth Scroll
-    ................................................. */
-	
-	$('a[href*="#"]:not([href="#"])').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-		  var target = $(this.hash);
-			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			  if (target.length) {
-				$('html,body').animate({
-				  scrollTop: target.offset().top - 65,
-				  }, 1000);
-				  return false;
-			  }
-		}
-	});
-
-	
-}(jQuery));
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}	
