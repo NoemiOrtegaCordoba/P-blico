678bcba49071bbb74dc703a55c713e3e817c2bdf
Noemí Ortega <noemiortegacordoba@gmail.com>
2021-05-28 19:45:50 +0200

    s
    s

--- scripts/colors.js
+++ scripts/colors.js
@@ -5,17 +5,23 @@ $(document).ready(function(){
 		$(".contact").css({"background-color": "#E5E8E8", "color": "black"});
 		$(".nav, .footer").css({"color": "white", "background-color": "#333"});
 		$(".nav a").css({"color": "white"});
+		$("#lele").css({"color": "black"});
 	});
 	
 	$("#contrast").click(function(){
 		$(".exposition, .a, .b, .contact").css({"background-color": "yellow", "color": "black"});
 		$(".nav, .footer").css({"color": "yellow", "background-color": "black"});
 		$(".nav a").css({"color": "yellow"});
+		$("#lele").css({"color": "black"});
+		$("#credits").css({"color": "yellow"});
 	});
   
 	$("#night").click(function(){
 		$(".exposition, .a, .b, .contact").css({"background-color": "#393939", "color": "white"});
 		$(".nav, .footer").css({"color": "white", "background-color": "#333"});
 		$(".nav a").css({"color": "white"});
+		$("#lele").css({"color": "white"});
+		$("#credits").css({"color": "white"});
 	});
 });
+
